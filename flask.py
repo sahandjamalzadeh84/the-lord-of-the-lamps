faaabd855d2927bb9605ff7b50a3aac1a1a8c0cf
Sadrasa833 <149875486+Sadrasa833@users.noreply.github.com>
2024-06-23 15:35:05 +0330

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify
-
+import queue
 
 app = Flask(__name__)
 
-
 # API key for authentication
-API_KEY = "your_api_key_here"
+API_KEY = "your_actual_api_key_here"  # مقدار کلید API را اینجا قرار دهید
 
 
 # Decorator to check for a valid API key in request headers
+
 def require_api_key(f):
     def decorated_function(*args, **kwargs):
         api_key = request.headers.get('api-key')
@@ -21,29 +21,31 @@ def require_api_key(f):
     decorated_function.__name__ = f.__name__
     return decorated_function
 
-
-request_queue = []
-
+# Initialize the queue for requests
+request_queue = queue.Queue()
+latest_status = {"status": "off"}
 
 @app.route('/requests', methods=['GET', 'POST'])
-@require_api_key
 def requests_handler():
     if request.method == 'GET':
-        if len(request_queue) == 0:
+        if request_queue.empty():
             return jsonify({'message': 'No requests in queue'}), 404
         else:
-            last_response = request_queue.pop(0)
+            last_response = request_queue.get()
             return jsonify(last_response)
-
 
     elif request.method == 'POST':
         data = request.get_json()
-        if data and 'state' in data:
-            request_queue.append(data)
-            return jsonify({'message': 'Request added to queue', 'queue_length': len(request_queue)})
+        if data and 'command' in data:  # Assuming the key 'command' is used in POST requests
+            request_queue.put(data)
+            latest_status["status"] = data['command']
+            return jsonify({'message': 'Request added to queue', 'queue_length': request_queue.qsize()})
         else:
-            return jsonify({'message': 'Invalid data or state key missing'}), 400
+            return jsonify({'message': 'Invalid data or command key missing'}), 400
 
+@app.route('/status', methods=['GET'])
+def get_status():
+    return jsonify(latest_status), 200
 
 if __name__ == '__main__':
     app.run(port=7000)
